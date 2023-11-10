7f8be34cd09044c2ccfa96f55d6e4c016ff3b162
sudheesh448 <sudheesh448@gmail.com>
2023-11-10 13:45:11 +0530

    404 page

--- src/Components/Common/NotFound.jsx
+++ src/Components/Common/NotFound.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Lottie from 'lottie-react';
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div>
+        <Lottie
+        animationData={notfound} // Replace with your animation data
+        loop={true} // Set to true to make the animation loop continuously
+        autoplay={true} 
+        className="w-1/3 ml-18 mt-24 "
+      />
+    </div>
   )
 }
 
