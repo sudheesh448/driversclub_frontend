dcb9d142d7f93d604d4bfac0991221ddde214815
sudheesh448 <sudheesh448@gmail.com>
2023-11-10 13:53:32 +0530

    404 page

--- src/Components/Common/NotFound.jsx
+++ src/Components/Common/NotFound.jsx
@@ -8,13 +8,15 @@ function NotFound() {
   return (
     <div>
         <Navbar/>
+        <div className='flex flex-col items-center justify-center min-h-screen'>
         <Lottie
         animationData={notfound} // Replace with your animation data
         loop={true} // Set to true to make the animation loop continuously
         autoplay={true} 
         className="w-1/3 ml-18 mt-24 "
-      />
-      <Footer/>
+        />
+        </div>
+        <Footer/>
     </div>
   )
 }
