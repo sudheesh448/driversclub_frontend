df3114df9b9f1b167116508b1b77f7f10ad9fb92
sudheesh448 <sudheesh448@gmail.com>
2023-11-15 21:14:56 +0530

    profile request history driver profile

--- src/Components/Driver/Driver_Home/Confirmed Request/ConfirmedRequest.jsx
+++ src/Components/Driver/Driver_Home/Confirmed Request/ConfirmedRequest.jsx
@@ -140,6 +140,89 @@ function ConfirmedRequest() {
           </div>
         </div>
       </div>
+
+      <div className='md:hidden mt-12'>
+      <div className='h-28 w-full mb-2 bg-slate-100'>
+            <Icons/>
+            </div>
+
+            <div className='w-full shadow-2xl '>
+
+<div className=" mt-2  flex w-full bg-slate-600">
+<div className='flex'>
+<img className="justify-center mt-0 py-0 w-9" src={bookmark} alt="" />
+<p className="text-2xl font-semibold mb-4 text-white">Confirmed Trip Requests</p>
+</div>
+</div>
+  <div className="shadow-xl">
+{confirmedRequests.length > 0 ? (
+confirmedRequests.map((trip) => (
+<div key={trip.id}  onClick={() => {
+navigate(`/trip_request_detail/${trip.trip_id}`);
+}} className="bg-sky-800 border  font-semibold grid md:grid-cols-5 gap-2 text-white cursor-pointer transform transition-transform hover:scale-105  hover:text-black hover:bg-orange-200">
+
+<div className='flex'>
+  <img  className='w-8' src={postBox} alt="" />
+<p className='ml-2'>{trip.user_first_name}</p>
+</div>
+<div className='grid '>
+    <div>
+    <p> {trip.from_location}</p>
+    </div>
+    <div className='justify-center flex w-52'>
+    <img className='w-8' src={fromto} alt="" />
+    </div>
+</div>
+<div>
+<p>
+   {trip.to_location}</p>
+</div>
+<div>
+<p> Date: {trip.journey_start_date}</p>
+</div>
+<div>
+ <p> Return: {trip.is_return ? "No return" : trip.return_date}</p>
+
+</div>
+
+{/* Add other trip details you want to display */}
+
+</div>
+))
+) : (
+<p className="col-span-5">No pending trip requests found.</p>
+)}
+</div>
+  <div className='pagination mt-2 flex justify-center '>
+  <button
+    onClick={() => changePage(currentPage - 1)}
+    disabled={currentPage === 1}
+    className={`bg-blue-500 cursor-pointer text-white p-2 rounded ${currentPage === 1 ? 'opacity-50 ' : ''}`}
+  >
+    Prev 
+  </button>
+  <span className="bg-sky-800 text-white font-bold p-2 rounded-full mx-4"> {currentPage}</span>
+  <button
+    onClick={() => {
+      if (confirmedRequests.length === 0) {
+        // Show a SweetAlert when confirmedRequests length is 0
+        Swal.fire('No more pages', 'No more confirmed trip requests found.', 'info');
+      } else {
+        changePage(currentPage + 1);
+      }
+    }}
+    disabled={confirmedRequests.length < perPage}
+    className={`bg-blue-500 cursor-pointer text-white p-2 rounded ${
+      confirmedRequests.length < perPage ? "opacity-50 " : ""
+    }`}
+  >
+    Next
+  </button>
+  </div>
+</div>
+
+
+      </div>
       <Footer />
     </>
   )
