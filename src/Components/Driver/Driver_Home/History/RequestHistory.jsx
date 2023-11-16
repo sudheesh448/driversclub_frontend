4c2a4b03cbcfd020500f5bfe971404deae69ab4d
sudheesh448 <sudheesh448@gmail.com>
2023-11-16 12:37:02 +0530

    profile request history driver profile

--- src/Components/Driver/Driver_Home/History/RequestHistory.jsx
+++ src/Components/Driver/Driver_Home/History/RequestHistory.jsx
@@ -145,7 +145,7 @@ function RequestHistory() {
     confirmedRequests.map((trip) => (
       <div key={trip.id}  onClick={() => {
         navigate(`/trip_request_detail/${trip.trip_id}`);
-      }} className="bg-sky-800 border p-4 font-semibold grid grid-cols-2 md:grid-cols-5 gap-2 text-white cursor-pointer transform transition-transform hover:scale-105  hover:text-black hover:bg-orange-200">
+      }} className="bg-sky-800 border p-4 font-semibold grid  md:grid-cols-5 gap-2 text-white cursor-pointer transform transition-transform hover:scale-105  hover:text-black hover:bg-orange-200">
         
         <div className='flex'>
           <img  className='w-8' src={postBox} alt="" />
